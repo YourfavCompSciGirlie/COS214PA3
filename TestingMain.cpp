089b361b97a4efee6666288777937c04bacb50ae
Malaika <u23618583@tuks.co.za>
2024-08-26 17:59:43 +0200

    Cleaned up the testing main...

--- TestingMain.cpp
+++ TestingMain.cpp
@@ -21,6 +21,7 @@
 
 #include "BattleStrategy.h"
 #include "TacticalMemento.h"
+#include "TacticalPlanner.h"
 #include "WarArchives.h"
 
 #include "Legion.h"
@@ -28,7 +29,7 @@
 
 using namespace std;
 
-
+// Abstract Factory
 void testFactory(LegionFactory* factory) {
     cout << "Testing factory: " << typeid(*factory).name() << endl;
 
@@ -65,6 +66,8 @@ void testFactory(LegionFactory* factory) {
 }
 
 
+
+// Strategy
 void testStrategy() {
     // Create factories for different terrains
     LegionFactory* woodlandFactory = new WoodlandFactory();
@@ -152,30 +155,92 @@ void testStrategy() {
 }
 
 
+
+// Memento
+void testMementoPattern() {
+    std::cout << "---- Testing Memento Pattern ----" << std::endl;
+
+    // Create different strategies
+    BattleStrategy* flanking = new Flanking();
+    BattleStrategy* fortification = new Fortification();
+    BattleStrategy* ambush = new Ambush();
+
+    // Create a TacticalPlanner
+    TacticalPlanner* planner = new TacticalPlanner();
+
+    // Create WarArchives (Caretaker)
+    WarArchives* archives = new WarArchives();
+
+    // Create TacticalCommand (Memento Client)
+    TacticalCommand* command = new TacticalCommand(flanking);
+
+    // Save initial state
+    planner->setStrategy(flanking);
+    TacticalMemento* memento1 = planner->createMemento();
+    archives->addTacticalMemento(memento1, "Flanking");
+
+    // Change strategy and save new state
+    planner->setStrategy(fortification);
+    TacticalMemento* memento2 = planner->createMemento();
+    archives->addTacticalMemento(memento2, "Fortification");
+
+    // Change strategy again
+    planner->setStrategy(ambush);
+
+    // Print current strategy
+    std::cout << "Current Strategy: Ambush" << std::endl;
+
+    // Restore the previous state
+    planner->restoreMemento(archives->getTacticalMemento("Fortification"));
+    command->setStrategy(planner->getStrategy());
+
+    // Print restored strategy
+    std::cout << "Restored Strategy: Fortification" << std::endl;
+    command->executeStrategy(/* some units */);
+
+    // Clean up
+    delete flanking;
+    delete fortification;
+    delete ambush;
+    delete planner;
+    delete archives;
+    delete command;
+}
+
+
+
+// Composite
 void testIndividualUnits() {
     std::cout << "---- Testing Individual Units ----" << std::endl;
     
-    Infantry infantry;
-    Cavalry cavalry;
-    Artillery artillery;
+    // Create concrete units
+    UnitComponent* infantry = new WoodlandInfantry();
+    UnitComponent* cavalry = new RiverbankCavalry();
+    UnitComponent* artillery = new OpenFieldArtillery();
 
-    infantry.move();
-    infantry.attack();
+    // Test move and attack methods
+    infantry->move();
+    infantry->attack();
 
-    cavalry.move();
-    cavalry.attack();
+    cavalry->move();
+    cavalry->attack();
 
-    artillery.move();
-    artillery.attack();
+    artillery->move();
+    artillery->attack();
+
+    // Clean up
+    delete infantry;
+    delete cavalry;
+    delete artillery;
 }
 
 void testCompositeLegion() {
     std::cout << "\n---- Testing Composite Legion ----" << std::endl;
 
     // Create individual units
-    UnitComponent* infantry1 = new Infantry();
-    UnitComponent* cavalry1 = new Cavalry();
-    UnitComponent* artillery1 = new Artillery();
+    UnitComponent* infantry1 = new WoodlandInfantry();
+    UnitComponent* cavalry1 = new RiverbankCavalry();
+    UnitComponent* artillery1 = new OpenFieldArtillery();
 
     // Create a composite legion
     Legion* legion = new Legion();
@@ -196,8 +261,8 @@ void testNestedLegions() {
     std::cout << "\n---- Testing Nested Legions ----" << std::endl;
 
     // Create individual units
-    UnitComponent* infantry1 = new Infantry();
-    UnitComponent* cavalry1 = new Cavalry();
+    UnitComponent* infantry1 = new WoodlandInfantry();
+    UnitComponent* cavalry1 = new RiverbankCavalry();
 
     // Create first sub-legion
     Legion* subLegion1 = new Legion();
@@ -205,8 +270,8 @@ void testNestedLegions() {
     subLegion1->add(cavalry1);
 
     // Create second sub-legion with more units
-    UnitComponent* artillery1 = new Artillery();
-    UnitComponent* infantry2 = new Infantry();
+    UnitComponent* artillery1 = new OpenFieldArtillery();
+    UnitComponent* infantry2 = new OpenFieldInfantry();
     Legion* subLegion2 = new Legion();
     subLegion2->add(artillery1);
     subLegion2->add(infantry2);
@@ -252,6 +317,7 @@ void testRemoveFunctionality() {
 
 
 
+
 int main() {
 
     cout << "===== Abstract Factory Design Pattern Testing =====" << endl;
@@ -271,6 +337,10 @@ int main() {
 
     cout << "===== Strategy Design Pattern Testing =============" << endl;
 
+    testMementoPattern();
+
+    cout << "===== Memento Design Pattern Testing ==============" << endl;
+
     testStrategy();
 
     cout << "===== Composite Design Pattern Testing =============" << endl;
@@ -280,67 +350,14 @@ int main() {
     testNestedLegions();
     testRemoveFunctionality();
 
-    cout << "===== End of All Tests =====" << endl;
+    cout << "===== End of All Tests =====" << endl << endl;
 
-
-
+    // Summary of All Tests
+    std::cout << "=== Summary of All Tests ===\n";
+    std::cout << "Abstract Factory Pattern: Tested creation of different military units based on terrain.\n";
+    std::cout << "Strategy Pattern: Tested different strategies and their application.\n";
+    std::cout << "Memento Pattern: Tested saving and restoring strategies using mementos.\n";
+    std::cout << "Composite Pattern: Tested adding and managing units in a composite structure.\n";
 
     return 0;
 }
-
-
-// // Testing main function
-// int main() {
-
-
-//     // Memento Pattern Testing
-//     std::cout << "=== Memento Pattern Testing ===\n";
-//     TacticalPlanner planner;
-//     WarArchives archives;
-    
-//     BattleStrategy* ambush = new Ambush();
-//     planner.setStrategy(ambush);
-//     TacticalMemento* memento = planner.createMemento();
-//     archives.addTacticalMemento(memento, "Ambush Strategy");
-
-//     // Restore strategy
-//     planner.restoreMemento(archives.getTacticalMemento("Ambush Strategy"));
-//     command.setStrategy(planner.createMemento()->getStoredStrategy());
-//     command.executeStrategy();
-
-//     delete ambush;
-
-//     std::cout << "=== End of Memento Pattern Testing ===\n\n";
-
-//     // Continue Composite Pattern Testing
-//     std::cout << "=== Composite Pattern Testing ===\n";
-//     Legion* legionComposite = new Legion();
-//     UnitComponent* infantryLeaf = new InfantryLeaf();
-//     UnitComponent* cavalryLeaf = new CavalryLeaf();
-//     UnitComponent* artilleryLeaf = new ArtilleryLeaf();
-
-//     legionComposite->add(infantryLeaf);
-//     legionComposite->add(cavalryLeaf);
-//     legionComposite->add(artilleryLeaf);
-
-//     legionComposite->move();
-//     legionComposite->fight();
-
-//     delete legionComposite;
-//     delete infantryLeaf;
-//     delete cavalryLeaf;
-//     delete artilleryLeaf;
-
-//     std::cout << "=== End of Composite Pattern Testing ===\n\n";
-
-//     // Summary of All Tests
-//     std::cout << "=== Summary of All Tests ===\n";
-//     std::cout << "Abstract Factory Pattern: Tested creation of different military units based on terrain.\n";
-//     std::cout << "Strategy Pattern: Tested different strategies and their application.\n";
-//     std::cout << "Memento Pattern: Tested saving and restoring strategies using mementos.\n";
-//     std::cout << "Composite Pattern: Tested adding and managing units in a composite structure.\n";
-
-//     std::cout << "=== End of All Tests ===\n";
-
-//     return 0;
-// }
